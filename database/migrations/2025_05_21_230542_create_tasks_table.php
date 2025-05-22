1d29d6b456759d8c350feecac634d3e04eb49a17
Uradgor <lavrenchyk27091990@gmail.com>
2025-05-22 12:01:03 +0300

    Створення seeders
    Заповнення тестовими даними
    php artisan migrate --seed

--- database/migrations/2025_05_21_230542_create_tasks_table.php
+++ database/migrations/2025_05_21_230542_create_tasks_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
+              $table->id();
+            $table->string('name');
+            $table->boolean('is_complited')->default(false);
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\Tasks;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        Tasks::factory(10)->create();
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+         User::factory(9)->create();
     }
 }
